a47183fde021d6a2bb32de0a70fc570acc6aec06
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 04:51:46 +0100

    Etape 2
    J'ai ici remplacé l'affichage des chiffres : on était en format "chiffres en file indienne" :
    1s
    2s
    ...
    1m
    
    On passe maintenant à un format où chaque seconde s'efface et est remplacée par la suivante sur la même ligne.

--- Programme_Horloge.py
+++ Programme_Horloge.py
@@ -1,15 +1,14 @@
 
-import time 
 
+import time
 def main():
-    s=0
-    try: #try-except : pour ameliorée l'affichage lorsqu'on interumpt le programme
-        while True :
-            s+=1 
-            print (s)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("You pressed Ctrl+C!")
-        print("-- Exiting--")        
-    
+    s = 0  # Initialisation des secondes
+    while True:
+        s += 1  # Incrémentation des secondes
+        if s == 60:  # Vérifie si 60 secondes sont atteintes
+            print("\r1 m", end="", flush=True)  # Affiche "1 m" et reste sur la même ligne
+            s = 0  # Réinitialise les secondes à 0
+        else:
+            print(f"\r{s}", end="", flush=True)  # Affiche les secondes en écrasant la précédente
+        time.sleep(1)  # Pause de 1 seconde
 main()
