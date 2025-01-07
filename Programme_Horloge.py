918d7b42084ddd8704fe52a97418f756b7ebd63f
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 05:28:47 +0100

    Etape 3
     Affichage des minutes et les secondes séparément dans le format 1m : 23s

--- Programme_Horloge.py
+++ Programme_Horloge.py
@@ -1,14 +1,15 @@
-
-
 import time
+
 def main():
-    s = 0  # Initialisation des secondes
+    seconds = 0  
+    minutes = 0  # Initialisation des minutes
     while True:
-        s += 1  # Incrémentation des secondes
-        if s == 60:  # Vérifie si 60 secondes sont atteintes
-            print("\r1 m", end="", flush=True)  # Affiche "1 m" et reste sur la même ligne
-            s = 0  # Réinitialise les secondes à 0
-        else:
-            print(f"\r{s}", end="", flush=True)  # Affiche les secondes en écrasant la précédente
+        seconds += 1  
+        if seconds == 60:  
+            minutes += 1  # Incrémente les minutes
+            seconds = 0  
+        
+        print(f"\r{minutes}m : {seconds}s", end="", flush=True)
         time.sleep(1)  # Pause de 1 seconde
+
 main()
