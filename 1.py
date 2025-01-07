879395a2c2e67e712c332384450253d2a4f434f1
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 05:10:40 +0100

    Brouillion

--- 1.py
+++ 1.py
@@ -1,15 +1,1 @@
-
-import time
-
-def main():
-    s = 0
-    while True:
-        s += 1 
-        if s == 60:  
-            print("\r1 m", end="", flush=True)  # Affiche "1 m" et reste sur la même ligne
-            s = 0  
-        else:
-            print(f"\r{s}", end="", flush=True)  # Affiche les secondes en écrasant la précédente
-        time.sleep(1) 
-
-main()
+#Brouillion
