a2c0360618ceb3bb3a0037763069f194012bf416
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 10:20:32 +0100

    Etape 5
    Incorporation des heures , ajustement du problème d'affichage ( mm ss ) : ( ce n'est plus 10 mais 20 caractères (avec les espaces))

--- Programme_Horloge.py
+++ Programme_Horloge.py
@@ -2,14 +2,19 @@ import time
 
 def main():
     seconds = 0  
-    minutes = 0  # Initialisation des minutes
+    minutes = 0  
+    heures = 0
     while True:
         seconds += 1  
         if seconds == 60:  
-            minutes += 1  # Incrémente les minutes
+            minutes += 1 
             seconds = 0  
-        print(f"\r{' ' * 10}", end="")  # Efface complètement la ligne(fin du 1m:9ss)
-        print(f"\r{minutes}m : {seconds}s", end="", flush=True)
-        time.sleep(1)  # Pause de 1 seconde
-
+        if minutes == 60:  # Vérifie si 60 minutes sont atteintes 
+            heures += 1  # Incrémente les heures
+            minutes = 0  # Réinitialise les minutes à 0
+    
+        print(f"\r{' ' * 20}", end="") 
+        # Efface complètement la ligne ( ligne de 20 ,on regle le probleme ss ou mm)
+        print(f"\r{heures}h : {minutes}m : {seconds}s", end="", flush=True)
+        time.sleep(1)       
 main()
