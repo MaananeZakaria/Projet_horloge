74c08b1e854bab575153a58f49c56b658a68a507
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 04:38:02 +0100

    Etape 1
    Je suis partie de la base vue en Kick Off , ici j'ai mit le block : TRY-EXCEPT afin que, plutôt que de montrer une erreur brute à l'écran, un message personnalisé s'affiche lorsqu'on souhaite arrêter le décompte avec Ctrl et C.

---
+++ Programme_Horloge.py
@@ -0,0 +1,15 @@
+
+import time 
+
+def main():
+    s=0
+    try: #try-except : pour ameliorée l'affichage lorsqu'on interumpt le programme
+        while True :
+            s+=1 
+            print (s)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C!")
+        print("-- Exiting--")        
+    
+main()
