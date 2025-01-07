047791157c417f69398da156fdcc3a14424f657e
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 05:42:33 +0100

    Etape 4
    Résolution du problème d'affichage  ex 1m:9ss

--- Programme_Horloge.py
+++ Programme_Horloge.py
@@ -8,7 +8,7 @@ def main():
         if seconds == 60:  
             minutes += 1  # Incrémente les minutes
             seconds = 0  
-        
+        print(f"\r{' ' * 10}", end="")  # Efface complètement la ligne(fin du 1m:9ss)
         print(f"\r{minutes}m : {seconds}s", end="", flush=True)
         time.sleep(1)  # Pause de 1 seconde
 
