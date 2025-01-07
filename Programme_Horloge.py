c20a77f9bde600a98e43e1661f3ea91d6752e7cb
Zakaria <zakaria.maanane@laplateforme.io>
2025-01-07 15:17:27 +0100

    Etape 6
    Intégration de 3 int(input) pour démarrer l'horloge à partir du temps souhaité

--- Programme_Horloge.py
+++ Programme_Horloge.py
@@ -1,20 +1,35 @@
 import time
 
 def main():
-    seconds = 0  
-    minutes = 0  
-    heures = 0
+    # Demander l'heure, les minutes et les secondes initiales
+    try:
+        heures = int(input("Entrez l'heure de départ (0-23) : "))
+        minutes = int(input("Entrez les minutes de départ (0-59) : "))
+        seconds = int(input("Entrez les secondes de départ (0-59) : "))
+    except ValueError:
+        print("Veuillez entrer des nombres valides.")
+        return
+    
+    # Verification et Validation des valeurs entrées
+    if not (0 <= heures < 24 and 0 <= minutes < 60 and 0 <= seconds < 60):
+        print("Les valeurs doivent respecter les plages : heures (0-23), minutes (0-59), secondes (0-59).")
+        return
+
+    
     while True:
         seconds += 1  
         if seconds == 60:  
-            minutes += 1 
-            seconds = 0  
-        if minutes == 60:  # Vérifie si 60 minutes sont atteintes 
-            heures += 1  # Incrémente les heures
-            minutes = 0  # Réinitialise les minutes à 0
-    
-        print(f"\r{' ' * 20}", end="") 
-        # Efface complètement la ligne ( ligne de 20 ,on regle le probleme ss ou mm)
-        print(f"\r{heures}h : {minutes}m : {seconds}s", end="", flush=True)
-        time.sleep(1)       
+            minutes += 1  
+            seconds = 0 
+        if minutes == 60:  
+            heures += 1  
+            minutes = 0  
+        if heures == 24:  # Vérifie si 24 heures sont atteintes
+            heures = 0  # Réinitialise les heures à 0
+        
+        # Efface complètement la ligne(l31) et affiche l'heure actuelle (l32)
+        print(f"\r{' ' * 20}", end="")
+        print(f"\r{heures:02d}h : {minutes:02d}m : {seconds:02d}s", end="", flush=True)
+        time.sleep(1)
+
 main()
